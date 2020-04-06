0cd1aff7ea4e513c7c8cb76b632640217582656b
Ashish Rao <ashishrao2598@gmail.com>
2020-04-06 09:40:40 +0530

    add smee client

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const axios = require('axios');
+const SmeeClient = require('smee-client')
 
+// smee webhook client
+const smee = new SmeeClient({
+  source: 'https://smee.io/mQAzpDaQKOQpXyr0',
+  target: 'http://localhost:3000/',
+  logger: console
+})
+
+// start forwarding to localhost
+smee.start()
+
+// express server
 const app = express();
+
+// payload
 const data = {
   "status": "There is power in the house💡"
 }
+
+// helper methods
 function sendReqBackToGlitch() {
   axios
     .post("https://crimson-north-ceiling.glitch.me", data)
@@ -16,10 +32,12 @@ function sendReqBackToGlitch() {
     });
 }
 
+// routes
 app.post('/', (req, res) => {
   sendReqBackToGlitch()
   console.log('request received from glitch')
   res.send('ok')
 })
 
+// run the server on port 3000
 app.listen('3000')
